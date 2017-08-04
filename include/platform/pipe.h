621f3fdca3c7315f333230950cab10b77e781b59
Trond Norbye <trond.norbye@gmail.com>
2017-08-04 09:18:39 +0200

    Inline everything
    Change-Id: I2f52b151ccca0f8d4be72aac3f19c47750579827

--- include/platform/pipe.h
+++ include/platform/pipe.h
@@ -108,7 +108,7 @@ public:
      *         write end)
      * @throws std::bad_alloc if memory allocation fails
      */
-    size_t ensureCapacity(size_t nbytes) {
+    inline size_t ensureCapacity(size_t nbytes) {
         const size_t tail_space = buffer.size() - write_head;
         if (tail_space >= nbytes) { // do we have enough space at the tail?
             return wsize();
@@ -156,7 +156,7 @@ public:
     /**
      * Get the current allocation size of the buffer
      */
-    size_t capacity() const {
+    inline size_t capacity() const {
         return buffer.size();
     }
 
@@ -164,7 +164,7 @@ public:
      * Read the number of bytes currently available in the read end
      * of the pipe
      */
-    size_t rsize() const {
+    inline size_t rsize() const {
         return getAvailableReadSpace().size();
     }
 
@@ -172,7 +172,7 @@ public:
      * Get the available read buffer (this may be used for simplicity
      * rather than calling consume to have to copy it out)
      */
-    cb::const_byte_buffer rdata() const {
+    inline cb::const_byte_buffer rdata() const {
         return getAvailableReadSpace();
     }
 
@@ -180,14 +180,14 @@ public:
      * Returns the number of bytes available to be written to in the
      * write end of the pipe
      */
-    size_t wsize() const {
+    inline size_t wsize() const {
         return getAvailableWriteSpace().size();
     }
 
     /**
      * Get the available write buffer
      */
-    cb::byte_buffer wdata() const {
+    inline cb::byte_buffer wdata() const {
         return getAvailableWriteSpace();
     }
 
@@ -200,7 +200,7 @@ public:
      *                 long
      * @return the number of bytes produced
      */
-    ssize_t produce(std::function<ssize_t(void* /* ptr */, size_t /* size */)>
+    inline ssize_t produce(std::function<ssize_t(void* /* ptr */, size_t /* size */)>
                             producer) {
         auto avail = getAvailableWriteSpace();
 
@@ -222,7 +222,7 @@ public:
      *                 provided buffer.
      * @return the number of bytes produced
      */
-    ssize_t produce(std::function<ssize_t(cb::byte_buffer)> producer) {
+    inline ssize_t produce(std::function<ssize_t(cb::byte_buffer)> producer) {
         auto avail = getAvailableWriteSpace();
 
         const ssize_t ret = producer({avail.data(), avail.size()});
@@ -237,7 +237,7 @@ public:
     /**
      * A number of bytes was made available for the consumer
      */
-    void produced(size_t nbytes) {
+    inline void produced(size_t nbytes) {
         if (write_head + nbytes > buffer.size()) {
             throw std::logic_error(
                     "Pipe::produced(): Produced bytes exceeds "
@@ -254,7 +254,7 @@ public:
      *                 The number of bytes consumed should be returned.
      * @return the number of bytes consumed
      */
-    ssize_t consume(std::function<ssize_t(const void* /* ptr */,
+    inline ssize_t consume(std::function<ssize_t(const void* /* ptr */,
                                           size_t /* size */)> consumer) {
         auto avail = getAvailableReadSpace();
         const ssize_t ret =
@@ -273,7 +273,7 @@ public:
      *                 returned.
      * @return the number of bytes consumed
      */
-    ssize_t consume(std::function<ssize_t(cb::const_byte_buffer)> consumer) {
+    inline ssize_t consume(std::function<ssize_t(cb::const_byte_buffer)> consumer) {
         auto avail = getAvailableReadSpace();
         const ssize_t ret = consumer({avail.data(), avail.size()});
         if (ret > 0) {
@@ -289,7 +289,7 @@ public:
      * could be invalidated (most likely reset to the beginning of the
      * buffer)
      */
-    void consumed(size_t nbytes) {
+    inline void consumed(size_t nbytes) {
         if (read_head + nbytes > write_head) {
             throw std::logic_error(
                     "Pipe::consumed(): Consumed bytes exceeds "
@@ -316,7 +316,7 @@ public:
      *
      * @return true if the buffer is empty after packing
      */
-    bool pack() {
+    inline bool pack() {
         if (read_head == write_head) {
             read_head = write_head = 0;
         } else {
@@ -334,21 +334,21 @@ public:
      * Is this buffer empty (the consumer end completely caught up with
      * the producer)
      */
-    bool empty() const {
+    inline bool empty() const {
         return read_head == write_head;
     }
 
     /**
      * Is this buffer full or not
      */
-    bool full() const {
+    inline bool full() const {
         return write_head == buffer.size();
     }
 
     /**
      * Clear all of the content in the buffer
      */
-    void clear() {
+    inline void clear() {
         write_head = read_head = 0;
     }
 
@@ -395,7 +395,7 @@ protected:
      * call produced() later on to mark the space as used and that it
      * should be available for the consumer in the read end of the pipe.
      */
-    cb::byte_buffer getAvailableWriteSpace() const {
+    inline cb::byte_buffer getAvailableWriteSpace() const {
         return {const_cast<uint8_t*>(buffer.data()) + write_head,
                 buffer.size() - write_head};
     }
@@ -406,7 +406,7 @@ protected:
      * use, and call consumed() later on to mark the space as free and
      * available for the producer.
      */
-    cb::const_byte_buffer getAvailableReadSpace() const {
+    inline cb::const_byte_buffer getAvailableReadSpace() const {
         return {buffer.data() + read_head, write_head - read_head};
     }
 
